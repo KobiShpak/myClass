1dd992ee92a3c9e418cc90e701d8f147bbbcfb09
Kobi Shpak <kobishpak@gmail.com>
2019-09-07 18:08:29 +0300

    added classesUserCanRegister GET request

--- src/routes/class.js
+++ src/routes/class.js
@@ -6,34 +6,58 @@ let router = express.Router()
 // GET localhost:3000/class
 router.get('/classes', (req, res) => {
     ClassModel.find({})
+        .then(doc => {
+            res.json(doc)
+        })
+        .catch(err => {
+            res.status(500), json(err)
+        })
+})
+
+// Get all classes of a user
+// GET localhost:3000/user?email=kobi@gmail.com
+router.get('/classesUserCanRegister', (req, res) => {
+    if (!req.query.email) {
+        return res.status(400).send('Missing URL parameter: email')
+    }
+
+    ClassModel.aggregate([{
+        $match: {
+            $not: {
+                students: req.query.email
+            }
+        }
+    }
+    ])
     .then(doc => {
         res.json(doc)
     })
     .catch(err => {
-        res.status(500),json(err)
+        res.status(500), json(err)
     })
 })
 
 // Get all classes of a user
 // GET localhost:3000/user?email=kobi@gmail.com
 router.get('/classesOfUser', (req, res) => {
-    if (!req.query.email){
+    if (!req.query.email) {
         return res.status(400).send('Missing URL parameter: email')
     }
 
     ClassModel.aggregate([
         {
-            $match:{
+            $match: {
                 $or: [
-                    {students: req.query.email},
-                    {teacher: req.query.email}
-                ]}
+                    { students: req.query.email },
+                    { teacher: req.query.email }
+                ]
+            }
         },
         {
-            $project:{
-                id: 1, 
-                name: 1, 
-                icon: 1 
+            $project: {
+                id: 1,
+                name: 1,
+                icon: 1
             }
         }
     ])
@@ -41,14 +65,14 @@ router.get('/classesOfUser', (req, res) => {
         res.json(doc)
     })
     .catch(err => {
-        res.status(500),json(err)
+        res.status(500), json(err)
     })
 })
 
 // Get a class
 // GET localhost:3000/class?id=1234
 router.get('/class', (req, res) => {
-    if (!req.query.id){
+    if (!req.query.id) {
         return res.status(400).send('Missing URL parameter: id')
     }
 
@@ -59,7 +83,7 @@ router.get('/class', (req, res) => {
         res.json(doc)
     })
     .catch(err => {
-        res.status(500),json(err)
+        res.status(500), json(err)
     })
 })
 
@@ -67,59 +91,59 @@ router.get('/class', (req, res) => {
 // Update a class
 // PUT localhost:3000/class?id=1234
 router.put('/class', (req, res) => {
-    if (!req.query.id){
+    if (!req.query.id) {
         return res.status(400).send('Missing URL parameter: id')
     }
-    
+
     ClassModel.findOneAndUpdate({
         id: req.query.id
     }, req.body, {
-        new: true
-    })
-    .then(doc => {
-        res.json(doc)
-    })
-    .catch(err => {
-        res.status(500),json(err)
-    })
+            new: true
+        })
+        .then(doc => {
+            res.json(doc)
+        })
+        .catch(err => {
+            res.status(500), json(err)
+        })
 })
 
 // Update a specific class field
 // PATCH localhost:3000/class?id=1234
 router.patch('/class', (req, res) => {
-    if (!req.query.id){
+    if (!req.query.id) {
         return res.status(400).send('Missing URL parameter: id')
     }
-    
+
     ClassModel.findOneAndUpdate({
         id: req.query.id
     }, req.body, {
-        new: true
-    })
-    .then(doc => {
-        res.json(doc)
-    })
-    .catch(err => {
-        res.status(500),json(err)
-    })
+            new: true
+        })
+        .then(doc => {
+            res.json(doc)
+        })
+        .catch(err => {
+            res.status(500), json(err)
+        })
 })
 
 // Delete a class
 // DELETE localhost:3000/class?id=1234
 router.delete('/class', (req, res) => {
-    if (!req.query.id){
+    if (!req.query.id) {
         return res.status(400).send('Missing URL parameter: id')
     }
-    
+
     ClassModel.findOneAndRemove({
         id: req.query.id
     })
-    .then(doc => {
-        res.json(doc)
-    })
-    .catch(err => {
-        res.status(500),json(err)
-    })
+        .then(doc => {
+            res.json(doc)
+        })
+        .catch(err => {
+            res.status(500), json(err)
+        })
 })
 
 
@@ -129,14 +153,14 @@ router.post('/class', (req, res) => {
     if (!req.body) {
         return res.status(400).send('Request body is missing')
     }
-        
+
     let model = new ClassModel(req.body)
     model.save()
-    .then(doc => {
-        if (!doc || doc.length === 0) {
-            return res.status(500).send(doc)
-        }
-        res.status(201).send(doc)
+        .then(doc => {
+            if (!doc || doc.length === 0) {
+                return res.status(500).send(doc)
+            }
+            res.status(201).send(doc)
         })
         .catch(err => {
             res.status(500).json(err)
